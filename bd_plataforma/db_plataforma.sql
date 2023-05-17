0a8bffd7c5418cb699e05cc3f996e5f6da857f9d
yan14082004 <130572079+yan14082004@users.noreply.github.com>
2023-05-17 13:22:14 -0400

    db_plataforma.sql

--- bd_plataforma/db_plataforma.sql
+++ bd_plataforma/db_plataforma.sql
@@ -1,57 +1,46 @@
 CREATE TABLE cliente 
 ( 
- nome_cliente VARCHAR(100) NOT NULL,  
- cliente_login VARCHAR(50) NOT NULL,    
- codigo_carro INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
- UNIQUE (cliente_login,codigo_carro: PK)
-); 
+ codigo_cliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+ nome_cliente VARCHAR(100) NOT NULL 
+);
 
-CREATE TABLE carro 
+CREATE TABLE administrador 
 ( 
- codigo_carro INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
- descricao VARCHAR(200) NOT NULL,  
- marca VARCHAR(50) NOT NULL,  
- modelo VARCHAR(20) NOT NULL,  
- ano INT NOT NULL,  
- UNIQUE (codigo_carro: PK)
-); 
-
-CREATE TABLE admin 
-( 
- codigo_admin INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  
- admin_login VARCHAR(100) NOT NULL AUTO_INCREMENT,   
- nome_admin VARCHAR(50) NOT NULL,  
- UNIQUE (codigo_admin: PK,login)
-); 
-
-CREATE TABLE aluguel 
-( 
- codigo_cliente INT PRIMARY KEY,  
- codigo_carro INT,  
- data DATE NOT NULL AUTO_INCREMENT,  
-); 
-
-CREATE TABLE mantem 
-( 
- codigo_admin INT PRIMARY KEY,  
- codigo_carro INT,  
+ codigo_administrador INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+ nome_administrador VARCHAR(100) NOT NULL 
 ); 
 
 CREATE TABLE login 
 ( 
- nome_completo_login VARCHAR(100) NOT NULL,  
- codigo_login INT NOT NULL AUTO_INCREMENT,  
- nome_login VARCHAR(50) NOT NULL,  
- senha_login VARCHAR(32) NOT NULL,  
- tipo login INT NOT NULL AUTO_INCREMENT,  
- codigo_cliente INT,  
- codigo_admin INT,  
- UNIQUE (codigo_login,nome_login,tipo login)
-); 
+ codigo_login INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+ nome_completo_login VARCHAR(100) NOT NULL,
+ nome_login VARCHAR(50) NOT NULL,
+ senha_login VARCHAR(32) NOT NULL UNIQUE,
+ tipo_login INT(11) NOT NULL DEFAuLT 0
+);
 
-ALTER TABLE aluguel ADD FOREIGN KEY(codigo_cliente) REFERENCES cliente (codigo_cliente)
-ALTER TABLE aluguel ADD FOREIGN KEY(codigo_carro) REFERENCES carro (codigo_carro)
-ALTER TABLE mantem ADD FOREIGN KEY(codigo_admin) REFERENCES admin (codigo_admin)
-ALTER TABLE mantem ADD FOREIGN KEY(codigo_carro) REFERENCES carro (codigo_carro)
-ALTER TABLE login ADD FOREIGN KEY(codigo_cliente) REFERENCES cliente (codigo_cliente)
-ALTER TABLE login ADD FOREIGN KEY(codigo_admin) REFERENCES admin (codigo_admin)
+CREATE TABLE carro 
+( 
+ codigo_carro INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+ descricao_carro VARCHAR(150) NOT NULL,
+ marca_carro VARCHAR(20) NOT NULL,
+ modelo_carro VARCHAR(20) NOT NULL,
+ ano_carro INT(4) NOT NULL
+);  
+
+CREATE TABLE aluguel 
+( 
+ codigo_aluguel INT NOT NULL AUTO_INCREMENT PRIMARY KEY,    
+ data DATE NOT NULL,  
+ descricao VARCHAR(200) NOT NULL,
+ codigoCarro INT NOT NULL,
+ codigoCliente INT NOT NULL,
+ FOREIGN KEY (codigoCarro)
+ REFERENCES carro(codigo_carro),
+ FOREIGN KEY (codigoCliente)
+ REFERENCES cliente(codigo_cliente)
+);
+
+
+INSERT INTO login (nome_completo_login, nome_login, senha_login, tipo_login)
+VALUES('administrador', 'admin123', 'banana', 0);
