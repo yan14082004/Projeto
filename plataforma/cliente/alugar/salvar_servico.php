2cd42503f12e9245f6d2bc9031117cb42b96a599
yan14082004 <130572079+yan14082004@users.noreply.github.com>
2023-06-04 16:40:54 -0400

    Add files via upload

---
+++ plataforma/cliente/alugar/salvar_servico.php
@@ -0,0 +1,56 @@
+<?php require('../../conexao.php');
+
+
+if (isset($_POST['submit'])) {      
+
+        //print_r($_POST['codigoCarro']);
+        //print_r("<br>");
+        //print_r($_POST['descricao']);
+        $codigoCarro = $_POST['codigoCarro'];
+
+        $descricao = $_POST['descricao'];
+
+        $codigoCliente = $_POST['codigoCliente'];
+
+
+        $insert_aluguel = "INSERT INTO aluguel (codigoCarro, descricao, codigoCliente) VALUES ('".$codigoCarro."','".$descricao."', '".$codigoCliente."')";	
+
+        $insert_descricao_carro = "UPDATE carro SET descricao_carro = 'ALUGADO' WHERE codigo_carro = $codigoCarro";
+
+        if (mysqli_query($conexao, $insert_aluguel)) {
+
+                //mysqli_close($conexao);
+
+                echo "<script> alert ('ALUGUEL FEITO COM SUCESSO!');</script>";
+
+                echo "<script> window.location.href='$url_cliente/alugar/alugar.php';</script>";
+                
+        } else {
+        
+                echo "<script> alert ('ERRO: NÃO FOI POSSÍVEL ALUGAR.');</script>";
+
+                echo "<script> window.location.href='$url_cliente/alugar/alugar.php';</script>";
+                
+                //mysqli_close($conexao);
+        }
+
+
+        if (mysqli_query($conexao, $insert_descricao_carro)) {
+
+                mysqli_close($conexao);
+
+                echo "<script> alert ('ALUGUEL FEITO COM SUCESSO!');</script>";
+
+                echo "<script> window.location.href='$url_cliente/alugar/alugar.php';</script>";
+                
+        } else {
+        
+                echo "<script> alert ('ERRO: NÃO FOI POSSÍVEL ALUGAR.');</script>";
+
+                echo "<script> window.location.href='$url_cliente/alugar/alugar.php';</script>";
+                
+                mysqli_close($conexao);
+        }       
+}
+
+?>
