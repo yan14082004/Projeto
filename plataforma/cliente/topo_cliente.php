d6d62978a21ee651f2ed4a2844158128ac40773b
yan14082004 <130572079+yan14082004@users.noreply.github.com>
2023-06-06 18:15:21 -0400

    Update topo_cliente.php

--- plataforma/cliente/topo_cliente.php
+++ plataforma/cliente/topo_cliente.php
@@ -9,7 +9,7 @@
 
 		echo "<script> alert ('ERRO: É NECESSÁRIO FAZER LOGIN');</script>";
 		
-		echo "<script> window.location.href='http://localhost/plataforma';</script>";
+		echo "<script> window.location.href='https://projeto-dev-web-1pcc.onrender.com/plataforma/';</script>";
 
 	}	
 
@@ -26,7 +26,7 @@
 		unset ($_SESSION['url_admin']);
 		unset ($_SESSION['url_cliente']);
 
-		echo "<script> window.location.href='http://localhost/plataforma';</script>";				
+		echo "<script> window.location.href='https://projeto-dev-web-1pcc.onrender.com/plataforma/';</script>";				
 
 	} 
 ?>
